c8d1f6b20fc5e58271d4ebcfb9a16441a3e3ff7f
d-kuro <kurosawa7620@gmail.com>
2024-05-14 13:17:08 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 return {
-  color_scheme         = "Dracula+",
+  color_scheme         = "One Dark (Gogh)",
   font_size            = 10.0,
   font                 = wezterm.font 'UDEV Gothic',
   default_cursor_style = 'SteadyUnderline'
