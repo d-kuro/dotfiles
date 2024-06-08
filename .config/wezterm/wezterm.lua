6792017ce6726107197e5b41cb4c95e166891e30
d-kuro <kurosawa7620@gmail.com>
2024-06-08 09:36:04 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 return {
-  color_scheme         = "JetBrains Darcula",
+  color_scheme         = "OneDark (base16)",
   font_size            = 10.0,
   font                 = wezterm.font 'UDEV Gothic',
   default_cursor_style = 'SteadyUnderline'
